aaa10ab792bc9e786e284122df1ca98efd23a2eb
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-02-18 17:50:29 +0300

    feat: add prop for indicate which filter button is occured

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -2,9 +2,10 @@ import type { FilterType } from '../types/filter'
 
 interface IFilterButtons {
 	onFilterChange: (item: FilterType) => void
+	currentBtn: FilterType
 }
 
-export default function FilterButtons({onFilterChange}: IFilterButtons) {
+export default function FilterButtons({onFilterChange, currentBtn}: IFilterButtons) {
 
 
 	return (
@@ -12,7 +13,7 @@ export default function FilterButtons({onFilterChange}: IFilterButtons) {
 			{(["all", "active", "completed"] as FilterType[]).map(filterBtn => (
 				<button
 					key={filterBtn}
-					className='w-100 border mr-3 cursor-pointer hover:border-blue-400 ease duration-200'
+					className={`w-100 border mr-3 cursor-pointer hover:border-blue-400 ease duration-200 ${currentBtn === filterBtn ? 'bg-blue-400' : ''} `}
 					onClick={() => onFilterChange(filterBtn)}
 				>
 					{filterBtn}
