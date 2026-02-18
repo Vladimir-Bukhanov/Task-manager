396a27558c4dc9481ae8d25e953675eb52fb70a4
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-02-18 17:43:13 +0300

    style: add style for filtered buttons

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -8,10 +8,11 @@ export default function FilterButtons({onFilterChange}: IFilterButtons) {
 
 
 	return (
-		<div>
+		<div className='flex'>
 			{(["all", "active", "completed"] as FilterType[]).map(filterBtn => (
 				<button
 					key={filterBtn}
+					className='w-100 border mr-3 cursor-pointer hover:border-blue-400 ease duration-200'
 					onClick={() => onFilterChange(filterBtn)}
 				>
 					{filterBtn}
